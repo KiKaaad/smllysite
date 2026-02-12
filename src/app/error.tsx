9cb1cf17573de3e59fda25d99446536cb7ec9a17
Кикидза <kikaaads@gmail.com>
2026-02-12 19:35:56 +0700

    исправление ошибок

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     return (
         <main className="min-h-screen bg-[#050505] text-white overflow-x-hidden">
 
-            {/* 404 */}
+            {/* 500 */}
 
             {/* Динамический остров */}
 
